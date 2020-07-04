ba0f7f19f7d82e36c6f50c1352c91627def01f11
sash stasyk <sashman90@gmail.com>
2020-07-04 13:06:14 +0100

    Updates

---
+++ precache-manifest.d7a66b2bdcfc10bf048395c17b9e144e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc0ea343e1569e62cf7dab4aa35c45a",
+    "url": "/miniature_search/index.html"
+  },
+  {
+    "revision": "4a1fd05522b4b3f256fe",
+    "url": "/miniature_search/static/css/main.00d4951b.chunk.css"
+  },
+  {
+    "revision": "df4f4c5c7c768fae688e",
+    "url": "/miniature_search/static/js/2.eb193b72.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/miniature_search/static/js/2.eb193b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1fd05522b4b3f256fe",
+    "url": "/miniature_search/static/js/main.47d2ed7d.chunk.js"
+  },
+  {
+    "revision": "4ee45c90b182e1e80026",
+    "url": "/miniature_search/static/js/runtime-main.54247aec.js"
+  }
+]);
