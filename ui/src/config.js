ecb49f872018426f6c9944cba19cbc41756c26b1
sash stasyk <sashman90@gmail.com>
2020-07-06 16:59:53 +0100

    Adding ga and gtm

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,1 +1,4 @@
 export const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:3000";
+export const gaId = "UA-171713860-1";
+export const gtmId = "GTM-KPKGMGP";
+export const gtmAuth = "mB6JVEeoP7Fxi0Oft0y0Fw";
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import TagManager from "react-gtm-module";
+import { gtmId, gtmAuth, gaId } from "./config";
+import ReactGA from "react-ga";
+
+const tagManagerArgs = {
+  gtmId,
+  auth: gtmAuth,
+  preview: "env-1",
+};
+
+TagManager.initialize(tagManagerArgs);
+
+ReactGA.initialize("gaId");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
