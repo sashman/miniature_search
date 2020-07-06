b66052abcc5e1b8fdd7611a2ce1a62e26c6a211e
sash stasyk <sashman90@gmail.com>
2020-07-06 16:56:39 +0100

    Updates

--- precache-manifest.14a5118daa480804fbf48aae3b161574.js
+++ precache-manifest.14a5118daa480804fbf48aae3b161574.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "766b3744010c72aaa34327f86163206a",
+    "revision": "2cd68805db2e9ba6d144230cdcf07d81",
     "url": "/miniature_search/index.html"
   },
   {
-    "revision": "4bdfd72a18e9c03f08df",
+    "revision": "fd86e838f636eb216d4a",
     "url": "/miniature_search/static/css/main.00d4951b.chunk.css"
   },
   {
-    "revision": "2f66b450e7b68e700f04",
-    "url": "/miniature_search/static/js/2.c6ede79c.chunk.js"
+    "revision": "01622d36dc3fc9744ae0",
+    "url": "/miniature_search/static/js/2.555f1781.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/miniature_search/static/js/2.c6ede79c.chunk.js.LICENSE.txt"
+    "url": "/miniature_search/static/js/2.555f1781.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4bdfd72a18e9c03f08df",
-    "url": "/miniature_search/static/js/main.a430242f.chunk.js"
+    "revision": "fd86e838f636eb216d4a",
+    "url": "/miniature_search/static/js/main.46a444e0.chunk.js"
   },
   {
     "revision": "4ee45c90b182e1e80026",
