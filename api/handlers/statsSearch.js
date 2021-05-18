b5504ff0090a19889b72be48cf84d5611797da55
sashman <sashman90@gmail.com>
2021-05-18 11:32:25 +0100

    Adding battlescribe search

---
+++ api/handlers/statsSearch.js
@@ -0,0 +1,80 @@
+const config = require("./../config");
+
+module.exports = {
+  statsSearch: (esClient) => async (req, res) => {
+    const { term } = req.query;
+    console.log(term);
+
+    return res.json(await getResults(esClient, term));
+  },
+};
+
+const query = (input) => ({
+  query: {
+    bool: {
+      must: [
+        {
+          multi_match: {
+            query: input,
+            fields: ["name"],
+            operator: "and",
+          },
+        },
+        {
+          exists: {
+            field: "unit.name",
+          },
+        },
+      ],
+    },
+  },
+  aggs: {
+    profiles: {
+      terms: {
+        field: "unit.profileName.keyword",
+      },
+    },
+
+    wepons: {
+      terms: {
+        field: "weapons.profileName.keyword",
+      },
+    },
+  },
+});
+
+const getResults = async (esClient, term) => {
+  const body = query(term);
+
+  const results = await esClient.search({
+    index: "battlescribe",
+    filter_path: "hits.hits._source,hits.total,aggregations",
+    body,
+  });
+
+  return results.body.hits.hits.map(({ _source: { name, unit, weapons } }) => ({
+    name,
+    models: groupProfiles(unit),
+    weapons: groupProfiles(weapons),
+  }));
+};
+
+const groupProfiles = (unit) =>
+  unit.reduce((acc, profile) => {
+    const { profileName, name, value } = profile;
+
+    if (!profileName) {
+      return acc;
+    }
+
+    const currentModel = (acc && acc[profileName]) || {};
+    const updatedModel = {
+      ...currentModel,
+      [name]: value,
+    };
+
+    return {
+      ...acc,
+      [profileName]: updatedModel,
+    };
+  }, {});
--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const cors = require("cors");
 const config = require("./config");
 const { Client } = require("@elastic/elasticsearch");
@@ -7,14 +8,17 @@ const client = new Client({ node: config.elasticsearch_endpoint });
 const app = express();
 const { search } = require("./handlers/search");
 const { availableFilters } = require("./handlers/availableFilters");
+const { statsSearch } = require("./handlers/statsSearch");
 
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("combined"));
 app.get("/", (req, res) => res.send("ok"));
 app.post("/search", search(client));
 app.get("/available_filters", availableFilters(client));
+app.get("/stats/search", statsSearch(client));
 
 app.listen(port, () =>
   console.log(`App listening at http://localhost:${port}`)
